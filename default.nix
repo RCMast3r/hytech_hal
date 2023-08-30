b62cd3652aec570b7665c4bf162ab2c3f620847f
Ben Hall <rcmast3r1@gmail.com>
2023-08-30 16:49:49 -0400

    full yeet

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation rec {
   version = "0.1.0";
   src = ./.;
   nativeBuildInputs = [ cmake gcc-arm-embedded ];
-  propagatedBuildInputs = [ libopencm3_stm32f4 ];
+  propagatedBuildInputs = [ gcc-arm-embedded libopencm3_stm32f4 ];
   # dontPatch = true;
   # dontFixup = true;
   # dontStrip = true;
